a7c1e2b1cc7e92f6b0067f63e1f32785ee7f61b4
ArtamonovDen <brightcolors46@gmail.com>
2021-03-07 00:14:40 +0300

    First transaction

--- web/app/bar/views.py
+++ web/app/bar/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cocktail
 
 
 def index(request):
     if request.method == 'POST':
-        print(request.POST.get('url'))
-    context = {'data': 'this is your bar!'}
+        cocktail_name = request.POST.get('name', '')
+        if cocktail_name:
+            cocktail = Cocktail.objects.create(
+                name=cocktail_name
+                )
+            cocktail.save()
+        print(cocktail_name)
+        return redirect('/bar')
+
+    cocktails = Cocktail.objects.all()
+    context = {'data': 'this is your bar!', 'cocktails': cocktails}
     return render(request, 'bar/index.html', context)
